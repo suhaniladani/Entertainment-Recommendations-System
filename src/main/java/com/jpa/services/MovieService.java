6993834486fb315269448c6e8edb58681fc3e8db
Your Name Here <suhani.ladani@gmail.com>
2018-08-17 18:59:19 -0400

    cascade

--- src/main/java/com/jpa/services/MovieService.java
+++ src/main/java/com/jpa/services/MovieService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,6 +43,12 @@ public class MovieService {
 		}
 			
 
+	}
+	
+	@DeleteMapping("/api/movie/{mid}")
+	public void deleteMovie(
+			@PathVariable("mid") int mid) {
+		movieRepository.deleteById(mid);
 	}
 	
 
