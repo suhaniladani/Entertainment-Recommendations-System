5aab799eca090240e13b7ec6c2b81105a45bfb95
naomimachado <machado.m@husky.neu.edu>
2018-08-18 17:53:50 -0400

    aws edit

--- src/main/java/com/jpa/Cs5200ProjectApplication.java
+++ src/main/java/com/jpa/Cs5200ProjectApplication.java
@@ -2,10 +2,18 @@ package com.jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Cs5200ProjectApplication {
+public class Cs5200ProjectApplication extends SpringBootServletInitializer {
 
+	@Override
+	 protected SpringApplicationBuilder
+	  configure(SpringApplicationBuilder application) {
+	  return application.sources(Cs5200ProjectApplication.class);
+	 }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Cs5200ProjectApplication.class, args);
 	}
